24b0a60eab3ddeb1e2edf6ce37920df364d4034b
Saahil Claypool <saahil@claypools.org>
2020-11-18 13:03:14 -0500

    Double buffer movie rating for speed

--- Blazor/Pages/SessionRating.razor.cs
+++ Blazor/Pages/SessionRating.razor.cs
@@ -18,29 +18,49 @@ namespace Blazor.Pages {
 
         [Parameter] public string? SessionId { get; set; }
         [Parameter] public string? RatingId { get; set; }
-        private string? PosterPath { get; set; } = null;
 
-        private bool loading = true; 
+        private bool loading = true;
 
         public MovieInformationQueryDTO? informationQuery;
 
         private MovieInformationResponseDTO? MovieInfo { get; set; }
+        private List<Task<MovieInformationResponseDTO?>> MovieInfos { get; set; } = new();
 
         private string MovieTitle => MovieInfo?.MovieTitle ?? "Loading...";
         private string MovieSummary => MovieInfo?.MovieSummary ?? "Loading...";
 
         protected override async Task OnInitializedAsync() {
             await base.OnInitializedAsync();
-            await FetchTitle();
+            await NextTitle();
         }
 
-        private async Task FetchTitle() {
+        private async Task NextTitle() {
+            const int cached = 2;
+            if (MovieInfos.Count > 0) {
+                MovieInfos.Remove(MovieInfos[0]);
+            }
+            if (MovieInfos.Count < cached) {
+                foreach (var _ in Enumerable.Range(MovieInfos.Count, cached - MovieInfos.Count)) {
+                    MovieInfos.Add(FetchTitle());
+                }
+            }
+
+            var newMovieInfo = await MovieInfos[0];
+            if (newMovieInfo is not null && newMovieInfo.MovieId != MovieInfo?.MovieId) {
+                MovieInfo = newMovieInfo;
+                loading = false;
+            }
+            else {
+                await NextTitle();
+            }
+
+        }
+
+        private async Task<MovieInformationResponseDTO?> FetchTitle() {
             informationQuery = new() {
-                SessionId = SessionId
+                SessionId = SessionId!
             };
-            MovieInfo = await Http!.GetFromJsonAsync<MovieInformationResponseDTO>($"api/Session/{SessionId!}/Rating/");
-            PosterPath = MovieInfo?.PosterPartialPath;
-            loading = false;
+            return await Http!.GetFromJsonAsync<MovieInformationResponseDTO>($"api/Session/{SessionId!}/Rating/");
         }
 
         protected async Task OnUpvote() {
@@ -56,20 +76,20 @@ namespace Blazor.Pages {
         }
 
         private async Task HandleCommand(string type) {
-            if(loading) {
+            if (loading) {
                 return;
             }
 
-            var createCommand = new RatingDTO() { 
+            var createCommand = new RatingDTO() {
                 Type = type,
                 MovieId = MovieInfo!.MovieId
-            } ;
+            };
             loading = true;
             var response = await Http!.PostAsJsonAsync($"api/Session/{SessionId!}/Rating/", createCommand);
             if (response.IsSuccessStatusCode) {
                 var responseDTO = await response.Content.ReadFromJsonAsync<CreateRatingResponseDTO>();
                 Console.WriteLine($"Response: {responseDTO.ToJson()}");
-                await FetchTitle();
+                await NextTitle();
             }
         }
     }
